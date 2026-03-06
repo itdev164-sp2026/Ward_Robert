142b8cf5fe8f7a257d4c5b24f045de7d45576b84
wardr19 <wardr19@gmatc.matc.edu>
2026-03-06 09:57:31 -0600

    blog-post template

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -27,8 +27,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-      spaceId: process.env.SPACE_ID,
-      accessToken: process.env.ACCESS_TOKEN
+      spaceId: `${process.env.SPACE_ID}`,
+      accessToken: `${process.env.ACCESS_TOKEN}`
       },
     },
     `gatsby-plugin-image`,
